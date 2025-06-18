9ef0806b421e9379cde67f6ff4cc7318cf0e8244
sdell624 <sdell624@gmail.com>
2025-06-17 20:42:48 -0400

    default project

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    public DriveSubsystem() {}
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
+    }
+
+}
